2a99d4128d3ef1b214fe4ca0b9545c80b331b54b
Dave Mayo <dave_mayo@harvard.edu>
2026-08-11 11:29:47 -0400

    Basic fields

--- indexer/custom_resource_mapper.rb
+++ indexer/custom_resource_mapper.rb
@@ -1,14 +1,29 @@
 require_relative '../../as_arclight/indexer/lib/mappers/arclight_mapper'
 
 class CustomResourceMapper < Arclight::ResourceMapper
+  # Hate this but to guarantee consistent behavior with former EAD process,
+  # we need to run it through the same method
+  @serializer = EADSerializer.new
 
   def map
     # Call super to include the default mapping from ResourceMapper
     # Alternatively, remove the call to super and implement a complete mapping
     super
 
-    # pump the numbers!
-    map_field('total_component_count_is', @json['_total_components'].to_i * 1000)
+    map_field('unitid_ssm', (0..3).map {|i| @json["id_#{i}"]}.compact.join('.'))
+    map_field('title_html_tesm', sanitize_mixed_content(@json["title"]))
+
   end
 
+  def sanitize_mixed_content(content)
+    content = @serializer.remove_smart_quotes(content)
+    content.gsub!("<br>", "<br/>")
+    content.gsub!("</br>", '')
+
+    content = @serializer.escape_content(content)
+    content = @serializer.strip_p(content)
+
+    xlink_eles = %w{ arc archref bibref extptr extptrloc extref extrefloc linkgrp ptr ptrloc ref refloc resource title }
+    content = @serializer.add_xlink_prefix(content) if xlink_eles.any? { |word| content =~ /<#{word}\s/ }
+  end
 end
